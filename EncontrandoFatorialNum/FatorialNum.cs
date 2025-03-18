df3669ea77349b3d5884c379648302f1fa1d41f6
Joao-Ribeir0 <joaoneeto1709@gmail.com>
2025-03-18 16:37:21 -0300

    Add files via upload

---
+++ EncontrandoFatorialNum/FatorialNum.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+using System.Reflection;
+
+int numero;
+while (true)
+{
+    Console.WriteLine("Digite um número inteiro: ");
+
+    bool intNumero = int.TryParse(Console.ReadLine(), out numero);
+
+    if (intNumero)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Por favor, digite um número inteiro!");
+    }
+}
+
+BigInteger CalcularFatorial(int numero)
+{
+    if (numero == 0)
+    {
+        return 1;
+    }
+
+    return numero * CalcularFatorial(numero - 1);
+}
+
+Console.WriteLine($"O fatorial de {numero} é {CalcularFatorial(numero)}");
+
+      
