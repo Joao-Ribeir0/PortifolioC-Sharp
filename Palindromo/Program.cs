3d2cf59d22fcd683565c317746c1d46ec16c7eb2
Joao-Ribeir0 <joaoneeto1709@gmail.com>
2025-03-18 16:40:32 -0300

    Add files via upload

---
+++ Palindromo/Program.cs
@@ -0,0 +1,29 @@
+﻿string palavra;
+
+Console.WriteLine("Digite uma palavra: ");
+palavra = Console.ReadLine();
+
+bool VerificaPalindromo(string palavra)
+{
+    string palavraInvertida = "";
+    int tamanhoPalavra = palavra.Length;
+
+    for(int i = tamanhoPalavra - 1; i >= 0; i--)
+    {
+        palavraInvertida += palavra[i];
+    }
+
+    
+
+    return palavraInvertida == palavra;
+
+}
+
+if (VerificaPalindromo(palavra))
+{
+    Console.WriteLine($"A palavra '{palavra}' é um palíndromo");
+}
+else
+{
+    Console.WriteLine($"A palavra '{palavra}' não é um palíndromo");
+}
---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,30 @@
+﻿
+double numero;
+
+while (true)
+{
+    Console.WriteLine("Digite um número e verifique se ele é par ou ímpar: ");
+    bool isDouble = double.TryParse(Console.ReadLine(), out numero);
+
+    if (!isDouble)
+    {
+        Console.WriteLine("Digite somente um número");
+    }
+    else
+    {
+        ParOuImpar(numero);
+        break;
+    }
+}
+void ParOuImpar(double num)
+{
+    if(num % 2 == 0)
+    {
+        Console.WriteLine($"O número {num} é par");
+    }
+    else
+    {
+        Console.WriteLine($"O número {num} é ímpar");
+    }
+}
+
